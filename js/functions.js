8ca3163b048937d4b4f876c03c71af15ffccd297
IgorBolduev <igorbolduev1989@gmail.com>
2023-02-21 21:12:21 +0700

    добавляет функции

--- js/functions.js
+++ js/functions.js
@@ -1,8 +1,8 @@
 
 // Функция для проверки длины строки
 
-const getLengthStroke = function (verifyStroke, numberStroke) {
-  if (verifyStroke.length <= numberStroke) {
+const getLengthStroke = function (verifyString, numberString) {
+  if (verifyString.length <= numberString) {
     return true;
   }
   return false;
@@ -12,13 +12,28 @@ getLengthStroke();
 
 // Функция для проверки строки на палиндром
 
-const palindromeChecker = function(someStroke) {
-  const reverseStroke = someStroke.toLowerCase().split('').reverse().join('');
-  if (someStroke.toLowerCase() === reverseStroke) {
-    return true;
-  }
-  return false;
-};
+const palindromeChecker = (someString) => {
+  const upgradeString = someString
+    .toLowerCase()
+    .replaceAll(' ','');
 
+  let reverseString = '';
+  for (let i = upgradeString.length - 1; i >= 0; i--) {
+    reverseString += upgradeString.at(i);
+  }
+  return upgradeString === reverseString;
+};
 palindromeChecker();
 
+// Функция возвращающая числа
+
+
+const getNumber = function(someStroke) {
+  const number = someStroke.split('').Number().join('');
+  if (number === true) {
+    return number;
+  }
+  return NaN;
+};
+
+getNumber();
