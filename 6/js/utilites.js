5f5d2ba307d53d978f471d5aa32f2bf922238463
Keks <keks@htmlacademy.ru>
2023-03-13 19:31:59 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/utilites.js
@@ -0,0 +1,12 @@
+const getRandomNumber = (a, b) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const higher = Math.floor(Math.max(a, b));
+  const result = Math.random() * (higher - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+// Создаем генератор случ.элементов массива
+
+const getRandomArrayElement = (elements) => elements[getRandomNumber(0, elements.length - 1)];
+
+export { getRandomNumber, getRandomArrayElement };
